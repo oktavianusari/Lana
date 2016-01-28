4b775c7112715aef5aee63c993338628e228e23e
oktavianusari <oktavianus.ari@gmail.com>
2016-01-28 17:34:23 +0700

    Initial commit

---
+++ app/src/main/java/id/my/ari/lana/AppDetail.java
@@ -0,0 +1,12 @@
+package id.my.ari.lana;
+
+/**
+ * Created by ari on 1/18/2016.
+ */
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
